1323dfc475575c9776195e999171b740927e84ec
jay4codes <jay4codes@gmail.com>
2024-12-13 09:41:59 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import AssignmentList from './components/AssignmentList';
 import AddAssignment from './components/AddAssignment';
 function App() {
   return (
     <div className="App">
-<AssignmentList/>
-<AddAssignment/>
+      <AssignmentList/>
+      <AddAssignment/>
     </div>
   );
 }
